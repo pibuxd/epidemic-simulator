800cded4ab353804e14f7c1487667f64f6b765e9
jzajac04 <jzajac04@gmail.com>
2025-10-30 15:21:57 +0100

    Movement turn
    Only random chaotic people movement now.

--- src/main/scala/simulator/Main.scala
+++ src/main/scala/simulator/Main.scala
@@ -4,12 +4,30 @@ import simulator.people.*
 
 object Main{
   def main(args: Array[String]): Unit = {
+
+    // Simulation Preparation
     val start = System.currentTimeMillis()
-    val board: Board = Board(200,200,3)
+    val board: Board = Board(10,10,3)
     val after = System.currentTimeMillis()
     val time = after - start
     println(s"Board generation costed us $time miliseconds")
-    print(board.fields{1}{0}.neighbours{1}.size)
-    val people: Seq[Person] = Seq.empty
+
+    // println(board.fields{1}{0}.neighbours{1}.size)
+    val people: Seq[Person] = ((1 to 0) map (i => BasicPerson(i, i, false, board)))
+                                            ++ Some(BasicPerson(9, 9, true, board))
+
+    // Turns
+    def movement_turn(): Unit = {
+      board.fields.flatten.foreach(field => field.clear())
+      people.foreach(person => person.make_step())
+      // people.foreach(person => println(person.get_position()))
+    }
+
+    // Main loop
+    for (i <- 1 to 10) {
+      movement_turn()
+//      println(i)
+//      board.fields.flatten.foreach(field => field.inhabitants.foreach(inhab => println(inhab.get_position())))
+    }
   }
 }
--- src/main/scala/simulator/fields/Field.scala
+++ src/main/scala/simulator/fields/Field.scala
@@ -1,19 +1,21 @@
 package simulator.fields
 
 import simulator.Board
+import simulator.people.*
 
 trait Field {
-//  def check_in(person: Person): Unit
+  def clear(): Unit
+  def check_in(person: Person): Unit
 //  def infect_neighbours(): Unit
-//  def infect_inhabitants(): Unit
+//  def infect_inhabitants(): Unit 
   def get_position(): (Int, Int) = {
     position
   }
 
   def calculate_neighbours(board: Board, layer: Int): Unit
 
-//  protected val infected_number: Array[Int]
-//  protected val inhabitants: Seq[Person]
-  val neighbours: Array[Set[Field]]
+  protected val infected_number: Array[Int]
+  var inhabitants: Seq[Person] = Seq.empty
+  val neighbours: Array[IndexedSeq[Field]]
   protected val position: (Int, Int)
 }
